0ac4cdb301e8a29890f08650c76fc70fe5271264
daillet <tom.daille@gmail.com>
2020-10-07 11:36:40 +0200

    Exercice 2

--- TP01-AMIA/shapes/src/main/java/fr/dauphine/ja/daillethomas/shapes/Point.java
+++ TP01-AMIA/shapes/src/main/java/fr/dauphine/ja/daillethomas/shapes/Point.java
@@ -1,5 +1,7 @@
 package fr.dauphine.ja.daillethomas.shapes;
 
+import java.util.ArrayList;
+
 public class Point {
 	
 	private int x,y;
@@ -37,11 +39,41 @@ public class Point {
 	public String toString() {
 		return "(" + x + ", " + y + ")";
 	}
+	
+	public boolean isSameAs(Point p) {	
+		return this.getX() == p.getX() && this.getY() == p.getY();
+	}
+	
+	@Override
+	public boolean equals(Object o2) {
+		if (!(o2 instanceof Point)) {
+			return false;
+		}
+		
+		Point p2 = (Point) o2;
+		return x == p2.x && y == p2.y;
+	}
 
 	public static void main(String[] args) {
 		Point p = new Point(1,2);
 		System.out.println(p.x+" "+p.y);
 		System.out.println(p);
+		
+		//EXERCICE 2
+		System.out.println("EXERCICE 2");
+		Point p1=new Point(1,2);
+		Point p2=p1;
+		Point p3=new Point(1,2);
+		
+		System.out.println(p1==p2);
+		System.out.println(p1==p3);
+		
+		//q3.
+		System.out.println("Question 3");
+		ArrayList<Point> list = new ArrayList<Point>();
+		list.add(p1);
+		System.out.println(list.indexOf(p2));
+		System.out.println(list.indexOf(p3));
 	}
 
 }
